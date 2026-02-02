f8967921b5d47a222eee2fb495c324ced8e58e37
steffe <stefan.morin@live.se>
2026-02-02 23:59:08 +0100

    Initial commit

---
+++ model/go.mod
@@ -0,0 +1,5 @@
+module householdPlanner/model
+
+go 1.25.5
+
+require golang.org/x/crypto v0.47.0
---
+++ model/login.go
@@ -0,0 +1,18 @@
+package login
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+type user struct {
+	username string
+	hash strin
+	salt string
+}
+
+
+func CreateUser(username, password string) (*user, error) {
+	
+	
+	return nil, nil;
+}
