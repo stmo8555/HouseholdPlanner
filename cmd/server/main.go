c19e8a0db34fcb9e2f5d13b4c6d874d079cb8910
steffe <stefan.morin@live.se>
2026-03-15 22:19:15 +0100

    Changed from vanilla to gin

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
+    "github.com/gin-gonic/gin"
 )
 
 type LayoutData struct {
@@ -16,42 +16,48 @@ type Grocery struct {
 	Picked bool
 }
 
-var tpl *template.Template
+var r *gin.Engine
 
 func main() {
-	tpl, _ = template.ParseGlob("../../web/templates/*.html")
-	http.HandleFunc("/chores", choresHandleFunc)
-	http.HandleFunc("/groceries", groceriesHandleFunc)
-	http.HandleFunc("/", indexHandleFunc)
-	http.HandleFunc("/login", loginHandleFunc)
-	http.HandleFunc("/logout", loginHandleFunc)
-	http.HandleFunc("/recipes", recipesHandleFunc)
-	fs := http.FileServer(http.Dir("../../web/static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	r = gin.Default()
+	r.LoadHTMLGlob("../../web/templates/*.html")
 
-	http.ListenAndServe(":8080", nil)
+	r.GET("/chores", choresHandleFunc)
+	r.GET("/groceries", groceriesHandleFunc)
+	r.GET("/", indexHandleFunc)
+	r.GET("/login", loginHandleFunc)
+	r.POST("/login", loginHandleFunc)
+	r.GET("/logout", loginHandleFunc)
+	r.GET("/recipes", recipesHandleFunc)
+	r.Static("/static/", "../../web/static")
+	
+	r.Run()
 }
 
-func choresHandleFunc(w http.ResponseWriter, r *http.Request) {
+func choresHandleFunc(c *gin.Context) {
 	data := LayoutData{Title: "Chores", Data: nil}
-	tpl.ExecuteTemplate(w, "chores.html", data)
+	c.HTML(http.StatusOK, "chores.html", data)
 }
-func groceriesHandleFunc(w http.ResponseWriter, r *http.Request) {
+
+func groceriesHandleFunc(c *gin.Context) {
 	groceries := make([]Grocery, 0, 5)
 	groceries = append(groceries, Grocery{Amount: 5, Product: "Mjölk", Brand: "Arla", Unit: "kg", Store: "ICA", Picked: false})
 	groceries = append(groceries, Grocery{Amount: 1, Product: "Gurk", Brand: "Arla", Unit: "kg", Store: "ICA", Picked: true})
 	data := LayoutData{Title: "Groceries", Data: groceries}
-	tpl.ExecuteTemplate(w, "groceries.html", data)
+	c.HTML(http.StatusOK, "groceries.html", data)
 }
-func indexHandleFunc(w http.ResponseWriter, r *http.Request) {
+
+func indexHandleFunc(c *gin.Context) {
 	data := LayoutData{Title: "Home", Data: nil}
-	tpl.ExecuteTemplate(w, "index.html", data)
+	c.HTML(http.StatusOK, "index.html", data)
 }
-func loginHandleFunc(w http.ResponseWriter, r *http.Request) {
+
+func loginHandleFunc(c *gin.Context) {
 	data := LayoutData{Title: "login", Data: nil}
-	tpl.ExecuteTemplate(w, "login.html", data)
+	c.HTML(http.StatusOK, "login.html", data)
 }
-func recipesHandleFunc(w http.ResponseWriter, r *http.Request) {
+
+func recipesHandleFunc(c *gin.Context) {
 	data := LayoutData{Title: "Recipes", Data: nil}
-	tpl.ExecuteTemplate(w, "recipes.html", data)
+	c.HTML(http.StatusOK, "recipes.html", data)
 }
